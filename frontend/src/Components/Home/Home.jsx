21829c0280c0c07f6c14ce12ab18f4c0c955b135
Harshil173 <harshilpatadiya8@gmail.com>
2024-06-29 19:14:45 +0530

    Home Page design enhance

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -11,10 +11,27 @@ const Home = () => {
         className="header-image"
       />
       <div className="search-filters">
-        <input type="text" placeholder="Search..." id='searchbar' />
-        <div>Filter by:</div>
-        <div id='sameid'>Date of Availability</div>
-        <div id='sameid'>Location</div> 
+        <div style={{backgroundColor : '#E9E8FC',display:'flex',alignItems: 'center',borderRadius: '15px',width: '250px'}}>
+          <input type="text" placeholder="  Search..." id='searchbar' />
+          <img
+          src="/images/iconsearch.png" 
+          alt="Header"
+          className="header-image"
+          style={{width: '20px', height: '22px'}}
+          />
+        </div>
+        <div style={{color:'grey'}}>Filter by:</div>
+        <div id='sameid'>
+          Date of Availability
+        </div>
+        <div id='diffid' style={{display:'flex',alignItems:'center',justifyContent:'center',gap:'7px'}}>
+        <img
+          src="/images/iconsearch.png" 
+          alt="Header"
+          className="header-image"
+          style={{width: '20px', height: '20px'}}
+          />
+          Location</div> 
         <div id='sameid'>Pricing</div>  
       </div>
     </div>
